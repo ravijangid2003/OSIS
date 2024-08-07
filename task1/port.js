63aebdad5b55d5e6ff0b9a9a0bcf584c98f6a5d1
Ravi <139530852+ravijangid2003@users.noreply.github.com>
2024-08-07 17:22:37 +0530

    first commit

---
+++ task1/port.js
@@ -0,0 +1,19 @@
+// scroll section
+window.onscroll = () => {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+
+  // remove toggle icon and navbar when click navbar links (scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+// ________________________________
+// toggle navbbar icon
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
